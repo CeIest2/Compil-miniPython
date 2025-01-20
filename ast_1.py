47a074ebdc0bf3f9b25e6a4ab90c10cf21b8dc1a
Livie Romanet <livie.romanet@telecomnancy.eu>
2025-01-20 11:32:17 +0100

    file, start_funct, def, params -> OK

--- ast_1.py
+++ ast_1.py
@@ -8,36 +8,57 @@ def simplify_rules(parse_tree):
     if value == "<file>":
         ast = ASTNode("<file>")
         for child in parse_tree.children :
-            if child.value not in [38, 39]: # NEWLINE EOF
+            if child.value not in ["38", "39"]: # NEWLINE EOF
+                print("ici" + child.value)
                 ast.add_child(simplify_tree(child))
         return ast
 
     if value == "<start_func>":
-        ast = ASTNode("<function>")
+        ast = ASTNode("<FUNCTION>")
         for child in parse_tree.children:
             if child.value == "<def>":
-                ast.add_child(simplify_tree(child))
+                return (simplify_tree(child))
         return ast
 
     if value == "<def>":
-        name = parse_tree.children[1].value  # Nom de la fonction (40 : identifiant)
-        params = simplify_parameters(parse_tree.children[4])  # Paramètres
-        body = simplify_tree(parse_tree.children[-1])  # Corps (suite)
-        ast = ASTNode("<def>")
+        for child in parse_tree.children :
+            print (child.value)
+            if child.value == "40":
+                name = child.value  # Nom de la fonction (40 : identifiant)
+            elif child.value == "<first_para>":
+                child.value ='<parameters>'
+                params = simplify_tree (child)
+            elif child.value == "<suite>":
+                body = simplify_tree(child)  # Corps (suite)
+        ast = ASTNode("<function>")
         ast.add_child(ASTNode(name))
         ast.add_child(params)
         ast.add_child(body)
         return ast
-
-    if value == "<parameters>":
-        return simplify_parameters(parse_tree)
+    
+    if value == "<suite>":
+        ast = ASTNode("<body>")
+        for child in parse_tree.children:
+            ast.add_child(simplify_tree(child))
+        return ast
+    
+    if value =="<parameters>":
+        ast = ASTNode("<parameters>")
+        L=simplify_parameters(parse_tree,[])
+        for child in L :
+            ast.add_child(child)
+        return ast
 
     if value == "<start_stmt>":
-        ast= ASTNode("<statements>")
-        for child in parse_tree.children:
-            if child.value == "<stmt>":
-                ast.add_child(simplify_tree(child))
-        return ast    
+        if parse_tree.children[0].value == '^':
+            return None
+        else:
+            ast= ASTNode("<statements>")
+            for child in parse_tree.children:
+                print(child.value)
+                if child.value == "<stmt>":
+                    ast.add_child(simplify_tree(child))
+            return ast    
 
     if value == "<stmt>":
         if parse_tree.children[0].value == 9:               # if
@@ -83,18 +104,24 @@ def simplify_rules(parse_tree):
 
 
 
-def simplify_parameters(param_node):
+def simplify_parameters(param_node,L):
     """
     Simplifie une liste de paramètres.
     - param_node: nœud de paramètres dans l'arbre de dérivation.
     """
     if not param_node or param_node.value == "^":
-        return ASTNode("<parameters>")
-    ast = ASTNode("<parameters>")
+        return L
     for child in param_node.children :
-        if child.value ==40:
-            ast.add_child(simplify_tree(child))
-    return ast
+        if child.value == "40":
+            ast = simplify_tree(child)
+            L.append(ast)
+        elif child.value == "<parameters>":
+            #ast = simplify_parameters(child)
+            nested_params = simplify_parameters(child,L)
+            L=L+nested_params
+            # for nested_child in nested_params:  # Ajouter tous les identifiants imbriqués
+            #     L.append(nested_child)
+    return L
 
 
 
@@ -113,7 +140,7 @@ def simplify_tree(parse_tree):
     if not parse_tree:
         return None
     
-    if parse_tree.value in [38,36,37,31,14,15]:  # NEWLINE, BEGIN, END, ","", (, ) inutiles
+    if parse_tree.value in ["38","36","37","31","14","15"]:  # NEWLINE, BEGIN, END, ","", (, ) inutiles
         return None
     
     if not parse_tree.children:
@@ -125,12 +152,12 @@ def simplify_tree(parse_tree):
     simplified_node = simplify_rules(parse_tree)
 
     # Si aucune règle spécifique n'a été appliquée, simplifier récursivement
-    if not simplified_node:
-        simplified_node = ASTNode(parse_tree.value)
-        for child in parse_tree.children:
-            simplified_child = simplify_tree(child)
-            if simplified_child:  # Vérifie que le nœud enfant n'est pas None
-                simplified_node.add_child(simplified_child)
+    # if not simplified_node:
+    #     simplified_node = ASTNode(parse_tree.value)
+    #     for child in parse_tree.children:
+    #         simplified_child = simplify_tree(child)
+    #         if simplified_child:  # Vérifie que le nœud enfant n'est pas None
+    #             simplified_node.add_child(simplified_child)
 
     return simplified_node
 
